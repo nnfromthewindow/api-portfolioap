6998402a0e12a0a4152119b9c6084fd84ba068a1
Nicolas Nuccelli <nuccelli@hotmail.com>
2022-11-30 20:23:41 -0300

    commit inicial

---
+++ src/main/java/com/portfolioap/apiportfolio/ApiPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioap.apiportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolioap/apiportfolio/ApiPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioap.apiportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
